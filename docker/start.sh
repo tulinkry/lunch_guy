19725a8048c4afe1546a590cb4567e8eaf99823d
Krystof Tulinger <k.tulinger@seznam.cz>
2018-01-12 16:47:47 +0100

    locating the composer binary

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /usr/src/app && \
-php composer.phar --no-interaction install && \
+php /usr/bin/composer.phar --no-interaction install && \
 php bin/console server:run 0.0.0.0:80 && \
 echo "Serving on 0.0.0.0:80"
