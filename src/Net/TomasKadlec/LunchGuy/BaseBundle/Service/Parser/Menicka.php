46fff7e3c15c84fab714f13756815b99e13dba29
Krystof Tulinger <k.tulinger@seznam.cz>
2018-03-28 09:17:25 +0200

    adding a new parser

---
+++ src/Net/TomasKadlec/LunchGuy/BaseBundle/Service/Parser/Menicka.php
@@ -0,0 +1,90 @@
+<?php
+namespace Net\TomasKadlec\LunchGuy\BaseBundle\Service\Parser;
+
+/**
+ * Class Menicka
+ *
+ * Parser implementation for 
+ *
+ * @package Net\TomasKadlec\LunchGuy\BaseBundle\Service\Parser
+ */
+class Menicka extends AbstractParser
+{
+
+    protected static $selector = 'div.menicka';
+
+    public function isSupported($format)
+    {
+        return ($format == 'menicka');
+    }
+
+    public function supports()
+    {
+        return [ 'menicka' ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function parse($format, $data, $charset = 'windows-1250')
+    {
+        if (!$this->isSupported($format))
+            return new \RuntimeException("Format {$format} is not supported.");
+        $data = $this
+            ->getCrawler($data, 'windows-1250')
+            ->filter(static::$selector)
+            ->each(function($node) {
+                $date = \DateTime::createFromFormat(
+                    'j.n.Y',
+                    trim(
+                        explode(
+                            ' ',
+                            $node->filter('div.datum')->text()
+                        )[1]
+                    )
+                );
+
+                if($date->format('j.n.Y') === (new \DateTime())->format('j.n.Y')) {
+                    $results = [];
+
+                    $key = $food = $price = null;
+                    foreach($node->filter('div') as $tmp) {
+                        if(strpos($tmp->getAttribute('class'), 'nabidka_1') !== false ||
+                           strpos($tmp->getAttribute('class'), 'nabidka_2') !== false) {
+                            $food = trim($tmp->nodeValue);
+                            $key = strpos($tmp->getAttribute('class'), 'capitalize') === false ? static::KEY_MAIN : static::KEY_SOUPS;
+                        } else if (strpos($tmp->getAttribute('class'), 'cena') !== false) {
+                            $price = intval($tmp->nodeValue);
+                        }
+
+                        
+
+
+                        if(!is_null($food) && !is_null($price)) {
+                            $results[$key][] = array($food, $price);
+                            $food = $price = null;
+                        }
+                    }
+
+                    return $results;
+                }
+            });
+
+        return array_filter($data, function($row) {
+            return !empty($row);
+        })[0];
+    }
+
+    /**
+     * Transforms data from the crawler to an internal array
+     *
+     * @param $data
+     * @return array
+     */
+    protected function process($data) {
+        print_r($data);
+        return $data;
+    }
+}
+
+//Zeleninový salát se smaženými kuřecími stripsy 1,3,7,12
