7d01306ee8bec9a893f7b192e73a45a0e72d69fc
Krystof Tulinger <k.tulinger@seznam.cz>
2018-06-19 11:28:09 +0200

    improving docker performance

--- docker/start.sh
+++ docker/start.sh
@@ -2,5 +2,5 @@
 
 cd /usr/src/app && \
 php /usr/bin/composer.phar --no-interaction install && \
-php bin/console server:run 0.0.0.0:80 && \
-echo "Serving on 0.0.0.0:80"
+echo "Serving on 0.0.0.0:80" && \
+php bin/console server:run 0.0.0.0:80
