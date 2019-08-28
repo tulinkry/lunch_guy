0cd86662096ce41ed1d3d82804f54ec81d10836c
Krystof Tulinger <k.tulinger@seznam.cz>
2019-08-28 12:47:24 +0200

    updating parsers

--- src/Net/TomasKadlec/LunchGuy/BaseBundle/Service/Parser/Menicka.php
+++ src/Net/TomasKadlec/LunchGuy/BaseBundle/Service/Parser/Menicka.php
@@ -1,10 +1,11 @@
 <?php
+
 namespace Net\TomasKadlec\LunchGuy\BaseBundle\Service\Parser;
 
 /**
  * Class Menicka
  *
- * Parser implementation for 
+ * Parser implementation for
  *
  * @package Net\TomasKadlec\LunchGuy\BaseBundle\Service\Parser
  */
@@ -20,7 +21,7 @@ class Menicka extends AbstractParser
 
     public function supports()
     {
-        return [ 'menicka' ];
+        return ['menicka'];
     }
 
     /**
@@ -33,24 +34,25 @@ class Menicka extends AbstractParser
         $data = $this
             ->getCrawler($data, 'windows-1250')
             ->filter(static::$selector)
-            ->each(function($node) {
+            ->each(function ($node) {
                 $date = \DateTime::createFromFormat(
                     'j.n.Y',
                     trim(
                         explode(
                             ' ',
-                            $node->filter('div.datum')->text()
+                            $node->filter('div.nadpis')->text()
                         )[1]
                     )
                 );
 
-                if($date->format('j.n.Y') === (new \DateTime())->format('j.n.Y')) {
+                if ($date->format('j.n.Y') === (new \DateTime())->format('j.n.Y')) {
                     $results = [];
 
                     $key = $food = $price = null;
-                    foreach($node->filter('div') as $tmp) {
-                        if(strpos($tmp->getAttribute('class'), 'nabidka_1') !== false ||
-                           strpos($tmp->getAttribute('class'), 'nabidka_2') !== false) {
+                    foreach ($node->filter('div') as $tmp) {
+                        $key = strpos($tmp->parentNode->getAttribute('class'), 'polevka') === false ? static::KEY_MAIN : static::KEY_SOUPS;
+
+                        if (strpos($tmp->getAttribute('class'), 'polozka') !== false) {
                             $food = trim($tmp->nodeValue);
                             // alergens
                             $food = preg_replace('/\(?(\s*\d+[a-z]?\s*,)*(\s*\d+[a-z]?\s*)*\)?\s*$/', '', $food);
@@ -58,26 +60,27 @@ class Menicka extends AbstractParser
                             // other stuff
                             $food = preg_replace('/\(MB\s+[^)]*\)\s*$/', '', $food);
                             $food = preg_replace('/\(MB\s*$/', '', $food);
+                            $food = preg_replace('/^\d+\.\s*/', '', $food);
                             // weight
-                            $food = preg_replace('/\d+g\s*-\s*/', '', $food);
+                            //$food = preg_replace('/\d+g\s*-\s*/', '', $food);
                             // weight
                             $food = preg_replace('/^\s*\d+g\s*/', '', $food);
-                            $key = strpos($tmp->getAttribute('class'), 'capitalize') === false ? static::KEY_MAIN : static::KEY_SOUPS;
                         } else if (strpos($tmp->getAttribute('class'), 'cena') !== false) {
                             $price = intval($tmp->nodeValue);
                         }
 
-                        if(!is_null($food) && !is_null($price)) {
+                        if (!is_null($food) && !is_null($price)) {
                             $results[$key][] = array($food, $price);
                             $food = $price = null;
                         }
+
                     }
 
                     return $results;
                 }
             });
 
-        $data = array_filter($data, function($row) {
+        $data = array_filter($data, function ($row) {
             return !empty($row);
         });
         return empty($data) ? $data : $data[0];
@@ -89,7 +92,8 @@ class Menicka extends AbstractParser
      * @param $data
      * @return array
      */
-    protected function process($data) {
+    protected function process($data)
+    {
         return $data;
     }
 }
--- src/Net/TomasKadlec/LunchGuy/BaseBundle/Service/Parser/PankrackyRynek.php
+++ src/Net/TomasKadlec/LunchGuy/BaseBundle/Service/Parser/PankrackyRynek.php
@@ -15,13 +15,13 @@ class PankrackyRynek extends AbstractParser
 {
 
     protected $days = [
-        'pondělí',
-        'úterý',
-        'středa',
-        'čtvrtek',
-        'pátek',
-        'sobota',
-        'neděle',
+        'pondělí' => 'monday',
+        'úterý' => 'tuesday',
+        'středa' => 'wednesday',
+        'čtvrtek' => 'thursday',
+        'pátek' => 'friday',
+        'sobota' => 'saturday',
+        'neděle' => 'sunday',
     ];
 
     protected static $selector = '#k2Container .itemFullText';
@@ -37,14 +37,15 @@ class PankrackyRynek extends AbstractParser
         return ['pankrackyrynek'];
     }
 
-    public function parse($format, $data, $charset = 'UTF-8') {
+    public function parse($format, $data, $charset = 'UTF-8')
+    {
         if (!$this->isSupported($format))
             return new \RuntimeException("Format {$format} is not supported.");
         $data = $this
             ->getCrawler($data, $charset)
             ->filter(static::$selector)
             ->each(function (Crawler $node) {
-                return $node->children()->each(function(Crawler $child) {
+                return $node->children()->each(function (Crawler $child) {
                     return $child->html();
                 });
             });
@@ -59,19 +60,19 @@ class PankrackyRynek extends AbstractParser
      */
     protected function process($data)
     {
-        $data = array_filter($data[0], function($el) {
+        $data = array_filter($data[0], function ($el) {
             return !preg_match('/^\s*$/ui', $el);
         });
 
-        $data = array_reduce($data, function($acc, $el) {
-            if($acc->processing &&
+        $data = array_reduce($data, function ($acc, $el) {
+            if ($acc->processing &&
                 preg_match('/^\w+? (\d+\.\d+\.)/ui', $el) &&
-                !in_array(strtolower(preg_replace('/^(\w+?) \d+\.\d+\./ui', '\1', $el)), $this->days)) {
+                !in_array(strtolower(preg_replace('/^(\w+?) \d+\.\d+\./ui', '\1', $el)), array_keys($this->days))) {
                 $acc->processing = false;
                 return $acc;
             }
 
-            if(!$acc->processing && preg_replace('/^\w+? (\d+\.\d+\.).*/ui', '\1', $el) == date('j.n.')) {
+            if (!$acc->processing && mb_strtolower(preg_replace('/^(\w+?) \d+\.\d+\./ui', '\1', $el)) == array_flip($this->days)[mb_strtolower(date('l'))]) {
                 $acc->processing = true;
                 return $acc;
             }
@@ -90,12 +91,13 @@ class PankrackyRynek extends AbstractParser
                     $food = preg_replace("/(\d+)(,-|\s*Kč).*/ui", '', $food);
                     $food = preg_replace('/\((\d+\s*,)*(\s*\d+\s*)*\)\s*$/', '', $food);
                     $food = str_replace('• ', '', $food);
-                    $acc->menu[preg_match('/polévk/', $food) ? self::KEY_SOUPS : self::KEY_MAIN][] = [$food, $price];
+                    $key = preg_match('/polévk|\bkrém\b/ui', $food) ? self::KEY_SOUPS : self::KEY_MAIN;
+                    $acc->menu[$key][] = [$food, $price];
                 }
             }
 
             return $acc;
-        }, (object) [ 'processing' => false, 'menu' => []]);
+        }, (object)['processing' => false, 'menu' => []]);
 
         return $data->menu;
     }
